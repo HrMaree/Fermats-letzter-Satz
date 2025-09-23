8ed59aa2685265be01affe85fc83a9996673bd93
HrMaree <smaree@posteo.de>
2025-09-23 15:46:01 +0200

    Create loadGedanken.js
    Hier werden Einträge aus einer GoogleSheet gelesen, die aus einem Google-Formular kommen

---
+++ loadGedanken.js
@@ -0,0 +1,44 @@
+function loadGedankenSheet(tabName) {
+  const sheetUrl = `https://docs.google.com/spreadsheets/d/1H_5DD-T3VIRRcPMohU5dr8rKnQj8U1mOakuW1AKpBuw/gviz/tq?sheet=${tabName}`;
+
+  function handleResponse(response) {
+    if (response.isError()) {
+      document.getElementById("gedanken-content").innerText = "Fehler beim Laden der Gedanken.";
+      return;
+    }
+
+    const data = response.getDataTable();
+    const numRows = data.getNumberOfRows();
+    const numCols = data.getNumberOfColumns();
+
+    const title = data.getValue(0, 0);
+    document.getElementById("gedanken-title").innerHTML = `<h2>${title}</h2>`;
+
+    let html = "";
+
+    for (let i = 2; i < numRows; i++) {
+      const kapitel = data.getValue(i, 1); // Spalte B: Kapitel
+      const gedanke = data.getValue(i, 2); // Spalte C: Gedanke
+      const autor = numCols >= 4 ? data.getValue(i, 3) : "";
+
+      if (kapitel && gedanke) {
+        html += `
+          <section class="gedanken-block">
+            <h3>${kapitel}</h3>
+            <p class="gedanke">„${gedanke}“</p>
+            ${autor ? `<p class="autor">– ${autor}</p>` : ""}
+          </section>
+        `;
+      }
+    }
+
+    document.getElementById("gedanken-content").innerHTML = html;
+    if (window.MathJax) MathJax.typesetPromise();
+  }
+
+  google.charts.load('current', { packages: ['corechart'] });
+  google.charts.setOnLoadCallback(() => {
+    const query = new google.visualization.Query(sheetUrl);
+    query.send(handleResponse);
+  });
+}
